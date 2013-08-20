463e5c3fe88e86624ab196d3285ecfae675c5f02
biospank <fabio.petrucci@gmail.com>
2013-08-20 23:59:26 +0200

    layout draft

--- gen/com/wireless/lamp/R.java
+++ gen/com/wireless/lamp/R.java
@@ -21,7 +21,8 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int lamp=0x7f020000;
+        public static final int custom_button=0x7f020000;
+        public static final int lamp=0x7f020001;
     }
     public static final class id {
         public static final int btnTest=0x7f090001;
