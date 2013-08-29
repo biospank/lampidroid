7e4dd76f81fed1b28f7bb85f40fd1617c1972425
biospank <fabio.petrucci@gmail.com>
2013-08-30 00:36:23 +0200

    layout background

--- gen/com/wireless/lamp/R.java
+++ gen/com/wireless/lamp/R.java
@@ -22,17 +22,19 @@ public final class R {
     }
     public static final class drawable {
         public static final int custom_button=0x7f020000;
-        public static final int ic_alarm=0x7f020001;
-        public static final int ic_alarm_active=0x7f020002;
-        public static final int ic_edit=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int ic_location_found=0x7f020005;
-        public static final int ic_location_off=0x7f020006;
-        public static final int ic_location_search=0x7f020007;
-        public static final int ic_repeat=0x7f020008;
-        public static final int ic_repeat_active=0x7f020009;
-        public static final int ic_sms=0x7f02000a;
-        public static final int ic_sms_active=0x7f02000b;
+        public static final int function_border=0x7f020001;
+        public static final int ic_alarm=0x7f020002;
+        public static final int ic_alarm_active=0x7f020003;
+        public static final int ic_edit=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int ic_location_found=0x7f020006;
+        public static final int ic_location_off=0x7f020007;
+        public static final int ic_location_search=0x7f020008;
+        public static final int ic_repeat=0x7f020009;
+        public static final int ic_repeat_active=0x7f02000a;
+        public static final int ic_sms=0x7f02000b;
+        public static final int ic_sms_active=0x7f02000c;
+        public static final int summary_border=0x7f02000d;
     }
     public static final class id {
         public static final int icAlarm=0x7f090006;
@@ -106,7 +108,10 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
-        public static final int Summary=0x7f070002;
+        public static final int Function=0x7f070004;
+        public static final int Summary=0x7f070003;
+        public static final int Summary_SubSummary=0x7f070005;
+        public static final int TableRowItem=0x7f070002;
     }
     public static final class xml {
         public static final int settings=0x7f040000;
